bf7c37b7df2309902f9e1f53d4f3148b0d321ca5
molliean <molliean29@gmail.com>
2024-05-13 13:54:32 -0700

    code with comments removed

--- app.js
+++ app.js
@@ -1,57 +1,3 @@
-// /*-------------------------------- Constants --------------------------------*/
-// const numbersEls = document.querySelectorAll(".number")
-// console.log(numbersEls);
-// const operandEls = document.querySelectorAll('.operand');
-// console.log(operandEls);
-
-// /*-------------------------------- Variables --------------------------------*/
-// let firstNumber;
-// let operation;
-// let secondNumber;
-// /*------------------------ Cached Element References ------------------------*/
-
-
-
-// /*----------------------------- Event Listeners -----------------------------*/
-// operandEls.forEach(operandBtn => {
-//     operandBtn.addEventListener('click', function(e){
-//         if(e.target.innerText === '='){
-//             console.log(calculate(firstNumber, secondNumber, operation));
-//         } else {
-//             operation = e.target.innerText;
-//         };
-//     })
-// })
-
-// numbersEls.forEach(numberBtn => {
-//     numberBtn.addEventListener("click", (event) => {
-//       // This log is for testing purposes to verify we're getting the correct value
-//       //console.log(event.target);
-//       //console.dir(event.target)
-// ;      console.log(event.target.innerText);
-//       // Future logic to capture the button's value goes here...
-//       const number = event.target.innerText;
-//       if (firstNumber === undefined){
-//         firstNumber = parseInt(number);
-//       } else {
-//         secondNumber = parseInt(number); 
-//       }
-//     });
-//   });
-  
-// /*-------------------------------- Functions --------------------------------*/
-// function calculate(num1, num2, op){
-//     if(op === '*'){
-//         console.log(num1 * num2)
-//     } else if(op === '+'){
-//         console.log(num1 + num2)
-//     } else if(op === '-'){
-//         console.log(num1 - num2)
-//     } else{
-//         console.log(num1 / num2);
-//     }
-// }
-
 function add(x, y){
     return x + y;
 }
@@ -151,18 +97,3 @@ function renderDisplayEmpty(){
     selectedOperation = '';
     isFirstNum = false;
 }
-
-
-// ================= IDEAS TO ADD ON ========================
-
-
-// 1. Try to implement the logic to update the second number,  
-//  You'll have to update secondNumber variable, 
-// and you'll have to update the renderCalculatorDisplay to use that variable
-// and you'll have to update the renderAnswer to use that variable
-
-// 2. Implement the clear button
-// set the display to zero
-// reset your variables to the initial values
-
-// Extra challenge would, be trying having firstNumber or SecondNumber be multiple digits
