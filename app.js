92a11794bb9ff1e9e7d19edfc19ee07e249eaef2
molliean <molliean29@gmail.com>
2024-05-13 13:51:20 -0700

    added css and new functionality to calculator

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ let num2 = '';
 let isFirstNum = false;
 
 calculatorEl.addEventListener('click', function(e){
-    if(e.target.className === 'operand'){
+    if((e.target.className === 'operand')){
         if(!isFirstNum){
             isFirstNum = true;
         }
@@ -94,12 +94,38 @@ calculatorEl.addEventListener('click', function(e){
         renderAnswer();
 	} else if(e.target.classList.contains('number')){
         if(!isFirstNum){
-            num1 += e.target.innerText;
+            num1 += parseInt(e.target.innerText);
         } else{
-            num2 += e.target.innerText;
+            num2 += parseInt(e.target.innerText);
         }
         renderCalculatorDisplay();
     } else if(e.target.className === 'clear'){
+        renderDisplayEmpty();
+    }
+})
+
+function checkButtonType(btn, className){
+    return btn.classList.contains(className)
+}
+
+
+document.addEventListener('keypress', (e) => {
+    if('+-*/'.includes(e.key)){
+        if(!isFirstNum){
+            isFirstNum = true;
+        }
+        selectedOperation = e.key;
+        renderCalculatorDisplay();
+	} else if(e.key === '='){
+        renderAnswer();
+	} else if('1234567890'.includes(e.key)){
+        if(!isFirstNum){
+            num1 += parseInt(e.key);
+        } else{
+            num2 += parseInt(e.key);
+        }
+        renderCalculatorDisplay();
+    } else if(e.key === 'c'){
         renderDisplayEmpty();
     }
 })
@@ -109,7 +135,7 @@ function renderCalculatorDisplay(){
     if(!isFirstNum){
         displayEl.innerText = num1;
     } else{
-        displayEl.innerText = `${num1}${selectedOperation}${num2}`;
+        displayEl.innerText = `${num1} ${selectedOperation} ${num2}`;
     }
 }
 
